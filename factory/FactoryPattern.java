20c3a85a12ffdac403ae63be757f6631d50739e3
Артур <s862008@yandex.ru>
2026-01-23 13:53:53 +0300

    Update FactoryPattern.java

--- factory/FactoryPattern.java
+++ factory/FactoryPattern.java
@@ -1,3 +1,56 @@
+package factory
+/**
+ * Абстрактный класс Logistics  - определяет фабричный метод,
+ * который должны реализовать конкретные создатели
+ */
+abstract class Logistics {
+    
+    /**
+     * Фабричный метод - будет переопределен в подклассах
+     */
+    public abstract Transport createTransport();
+    
+    /**
+     * Бизнес-логика, которая использует фабричный метод
+     */
+    public void planDelivery() {
+        Transport transport = createTransport();
+        System.out.println("Стоимость доставки: $" + transport.getCost());
+        transport.deliver();
+    }
+}
+
+/**
+ * Конкретный создатель A - создает грузовики
+ */
+class RoadLogistics extends Logistics {
+    @Override
+    public Transport createTransport() {
+        return new Truck();
+    }
+}
+
+/**
+ * Конкретный создатель B - создает корабли
+ */
+class SeaLogistics extends Logistics {
+    @Override
+    public Transport createTransport() {
+        return new Ship();
+    }
+}
+
+/**
+ * Конкретный создатель C - создает самолеты
+ */
+class AirLogistics extends Logistics {
+    @Override
+    public Transport createTransport() {
+        return new Airplane();
+    }
+}
+
+
 public class FactoryPattern {
     public static void main(String[] args) {
         System.out.println("=== Демонстрация Простой Фабрики ===");
